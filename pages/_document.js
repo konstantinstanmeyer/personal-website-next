0d74f20a75c1ec15169693e922b719ae9467d47b
KonstantinStanmeyer <konstantin@stanmeyer.com>
2023-02-21 13:22:52 -0700

    default setup

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,10 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="icon" href="/side-profile.png" />
+        <title>Konstantin's Portfolio</title>
+      </Head>
       <body>
         <Main />
         <NextScript />
